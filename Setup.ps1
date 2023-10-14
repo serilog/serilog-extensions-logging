6e71b3381dd65c0b6a23b228a09973989ce5e249
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-10-14 13:30:38 +1000

    Update to .NET 8 RC2

---
+++ Setup.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "Stop"
+
+$RequiredDotnetVersion =  $(cat ./global.json | convertfrom-json).sdk.version
+
+New-Item -ItemType Directory -Force "./build/" | Out-Null
+
+Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "./build/installcli.ps1"
+& ./build/installcli.ps1 -InstallDir "$pwd/.dotnetcli" -NoPath -Version $RequiredDotnetVersion
+if ($LASTEXITCODE) { throw ".NET install failed" }
+
+$env:Path = "$pwd/.dotnetcli;$env:Path"
