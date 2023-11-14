85172ada1875cb29bfa072e7a97251623ac853b6
James Duda <jimbojim1997@gmail.com>
2023-11-14 18:08:29 +0000

    Changed copyright header from .NET Foundation to Serilog Contributors.

--- test/Serilog.Extensions.Logging.Tests/Support/LogEventPropertyFactory.cs
+++ test/Serilog.Extensions.Logging.Tests/Support/LogEventPropertyFactory.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright © Serilog Contributors
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Serilog.Core;
